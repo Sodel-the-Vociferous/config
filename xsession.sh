769a0b59e4b576dfbbb1438b1fbc8a892faa3d61
Daniel Ralston <Wubbulous@gmail.com>
2013-03-13 19:34:03 -0700

    Update syndaemon/synclient config

--- xsession.sh
+++ xsession.sh
@@ -13,7 +13,16 @@ WALLPAPER_FILENAME=~/config/wallpaper
 
 # Disable touchpad "tap" gestures while typing.
 # Idle time: 1.5s; ignore modifier keys
-which syndaemon && syndaemon -i 1.5 -t -k -d &
+already_running syndaemon && killall syndaemon
+
+which syndaemon &&
+syndaemon -i 1.5 -t -k -d &&
+synclient VertTwoFingerScroll=1 \
+    PalmDetect=1 \
+    TapButton1=1 \
+    TapButton2=2 \
+    TapButton3=3 \
+    PalmMinWidth=10
 
 numlockx & # Turn num lock on
 
