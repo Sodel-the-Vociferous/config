872d1d67d2d357ec60194ecd20a2a5199eb219f7
Daniel Ralston <Wubbulous@gmail.com>
2013-03-26 00:43:41 -0700

    Add script to configure some GNOME 3 stuff for me

---
+++ bin/setup-gnome3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Desktop background
+BACKGROUND_REAL_PATH=$(readlink -f ~/config/background)
+gsettings set org.gnome.desktop.background picture-uri "file://$BACKGROUND_REAL_PATH"
+
+# Stop GNOME from overwriting my synclient settings after the system
+# resumes from suspension
+which synclient &&
+gsettings set org.gnome.settings-daemon.plugins.mouse active false
