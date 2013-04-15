6bd049303a977765972f2ef79461b4f0bb6d85dd
Daniel Ralston <Wubbulous@gmail.com>
2013-04-15 07:42:22 -0700

    Make tmux-client.sh more readable

--- bin/tmux-client.sh
+++ bin/tmux-client.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+not_in_tmux_or_screen () {
+    [[ -z $TMUX ]] &&
+    [[ $TERM != screen ]] &&
+    [[ $TERM != screen-256color ]]
+}
+
 no_tmux_clients () {
-    [[ $(tmux list-sessions | wc -l) -lt 2 ]]
+    [[ ! "$(tmux list-sessions | egrep -i 'attached.$')" ]]
 }
 
-tmux_client () {
-    no_tmux_clients && tmux new-session -t TMUX_BG ||
-    tmux new-session -t TMUX_BG \; new-window -c $(pwd)
+new_tmux_client () {
+    if no_tmux_clients
+    then
+        exec tmux new-session -t TMUX_MASTER
+    else
+        exec tmux new-session -t TMUX_MASTER \; new-window -c $(pwd)
+    fi
 }
 
-[[ $TERM != screen ]] &&
-[[ $TERM != screen-256color ]] &&
-(tmux new-session -A -s TMUX_BG \; detach
-tmux_client)
+not_in_tmux_or_screen &&
+tmux new-session -A -s TMUX_MASTER \; detach &&
+new_tmux_client
