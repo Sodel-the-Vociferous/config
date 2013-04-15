7c0c900dd94ac7af39fe3416c60778f3ee16fadf
Daniel Ralston <Wubbulous@gmail.com>
2013-04-15 12:08:11 -0700

    Replace netcfg with netctl

--- bin/setup-system.sh
+++ bin/setup-system.sh
@@ -4,13 +4,15 @@
 sudo systemctl enable atd
 sudo systemctl enable avahi-daemon
 sudo systemctl enable cronie
-sudo systemctl enable net-auto-wired
-sudo systemctl enable net-auto-wireless
+sudo systemctl enable netctl
+sudo systemctl enable netctl-ifplugd
 sudo systemctl enable ntpd
 sudo systemctl enable osspd
 sudo systemctl enable remote-fs.target
 sudo systemctl enable rpcbind
 sudo systemctl enable sshd
 sudo systemctl enable syslog-ng
+
+sudo netctl enable RalstonNet
 
 sudo cp -vr ~/config/etc/* /etc
