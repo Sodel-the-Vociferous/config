4b379ea2eca09e0291cc6e6df2fd4fe85fc3e791
Daniel Ralston <Wubbulous@gmail.com>
2013-04-19 02:04:36 -0700

    Make tmux use 256 colors

--- bin/tmux-client.sh
+++ bin/tmux-client.sh
@@ -7,21 +7,21 @@ not_in_tmux_or_screen () {
 }
 
 no_tmux_clients () {
-    [[ ! "$(tmux list-sessions | egrep -i 'attached.$')" ]]
+    [[ ! "$(tmux -2 list-sessions | egrep -i 'attached.$')" ]]
 }
 
 new_tmux_client () {
     if no_tmux_clients
     then
-        exec tmux new-session -t TMUX_MASTER \; \
+        exec tmux -2 new-session -t TMUX_MASTER \; \
             set -q destroy-unattached on
     else
-        exec tmux new-session -t TMUX_MASTER \; \
+        exec tmux -2 new-session -t TMUX_MASTER \; \
             new-window -c $(pwd) \; \
             set -q destroy-unattached on
     fi
 }
 
 not_in_tmux_or_screen &&
-tmux new-session -A -s TMUX_MASTER \; detach &&
+tmux -2 new-session -A -s TMUX_MASTER \; detach &&
 new_tmux_client
