9963096cb605747ea89f8bf08c1110b8886c87e3
Daniel Ralston <Wubbulous@gmail.com>
2013-07-17 10:26:33 -0700

    Update pkgs and Fedora setup

--- bin/fedora-setup.sh
+++ bin/fedora-setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Setup my system-wide configuration, tailered to Fedora
 
+sudo cp -v --no-preserve=mode,ownership -r ~/config/etc/* /etc
+
+# Enable RPM Fusion repo
+rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-19.noarch.rpm
+
+sudo yum install $(cat ~/config/pkgs/*.lst) --skip-broken
+
 sudo systemctl enable atd
 sudo systemctl enable avahi-daemon
 sudo systemctl disable lightdm
@@ -9,10 +16,3 @@ sudo systemctl enable ntpd
 sudo systemctl enable remote-fs.target
 sudo systemctl enable rpcbind
 sudo systemctl enable dovecot
-
-sudo cp -v --no-preserve=mode,ownership -r ~/config/etc/* /etc
-
-# Enable RPM Fusion repo
-rpm -ivh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-19.noarch.rpm
-
-sudo yum install $(cat ~/config/pkgs/*.lst)
