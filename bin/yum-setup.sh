4fcabf969bcb10272c581bc9a5bcce4c080b6214
Daniel Ralston <daniel@dralston.net>
2013-08-14 22:41:39 -0700

    add adobe repo to yum conf script

--- bin/yum-setup.sh
+++ bin/yum-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo yum-config-manager --add-repo=http://www.beduine.de/repository/beduine.repo
-sudo yum localinstall --nogpgcheck \
+sudo yum install --nogpgcheck \
     http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+    http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm -y
