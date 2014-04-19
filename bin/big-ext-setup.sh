24ff709d570a94e73f4ac1d43ac67cca22946d15
Daniel Ralston <daniel@dralston.net>
2014-04-19 02:06:34 -0700

    fix symlink paths

--- bin/big-ext-setup.sh
+++ bin/big-ext-setup.sh
@@ -7,4 +7,4 @@ then
 fi
 
 mkdir /media/big-ext
-echo "/dev/disk/by-label/BigExt /media/big-ext              ext4    defaults        0 0" | tee -a /etc/fstab
+echo "/dev/disk/by-label/BigExt /media/big-ext              ext4    defaults,auto   0 0" | tee -a /etc/fstab
