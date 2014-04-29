eeddd422712ce6ca25bb7f77e7f8aa1fcf34eb35
Daniel Ralston <daniel@dralston.net>
2014-04-28 20:07:24 -0700

    enable ssh daemon

--- bin/services-setup.sh
+++ bin/services-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+sudo systemctl disable lightdm
 sudo systemctl enable atd
 sudo systemctl enable avahi-daemon
-sudo systemctl disable lightdm
+sudo systemctl enable dovecot
 sudo systemctl enable gdm
 sudo systemctl enable ntpd
 sudo systemctl enable remote-fs.target
 sudo systemctl enable rpcbind
-sudo systemctl enable dovecot
