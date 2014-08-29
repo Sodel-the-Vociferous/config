5e3a7158991a186f6448033d43e96be910591453
Daniel Ralston <daniel@dralston.net>
2014-08-28 22:09:45 -0700

    configure helm (YEEES)

--- emacs.el
+++ emacs.el
@@ -551,3 +551,22 @@ to agenda files."
           (lambda ()
             (add-hook 'electric-indent-functions
                       (lambda (_unused) 'no-indent) nil 'local)))
+
+;;; Helm
+(setq helm-command-prefix "C-z h")
+(require 'helm-config)
+(helm-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-h a") 'helm-apropo)
+(define-key user-map (kbd "<C-SPC>") 'helm-all-mark-rings)
+
+(define-key helm-map (kbd "x") 'helm-register)
+(define-key helm-map (kbd "<Tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
