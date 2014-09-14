166a48d316e1986b39b938f03761cb55d4e20ce8
Daniel Ralston <daniel@dralston.net>
2014-09-13 19:50:32 -0700

    Help cygwin close properly w/ backgd processes

--- login-scripts/bash_logout.sh
+++ login-scripts/bash_logout.sh
@@ -5,3 +5,14 @@
 if [ "$SHLVL" = 1 ]; then
     [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
 fi
+
+
+if [[ $OS_TYPE = cygwin ]]
+then
+	ps |
+	tail -n+2 |
+	sed -re 's/  +/ /g' |
+	cut -d ' ' -f2 |
+	xargs.exe kill
+fi
+
