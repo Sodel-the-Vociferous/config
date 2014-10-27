2bb06f8be27aa20f500269f5620ad3559732e36b
Daniel Ralston <daniel@dralston.net>
2014-10-26 21:10:44 -0700

    Update melpa URL

--- emacs.el
+++ emacs.el
@@ -41,7 +41,7 @@
 
 ;; Setup package archives
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
-                         ("MELPA" . "http://melpa.milkbox.net/packages/")
+                         ("MELPA" . "http://melpa.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;; Load packages
