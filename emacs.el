d182d65c71a10b04d1d7da5b3675546bafa25e03
Daniel Ralston <daniel@dralston.net>
2014-10-29 16:21:23 -0700

    Make 'N' mark selected feed as read in elfeed

--- emacs.el
+++ emacs.el
@@ -223,6 +223,12 @@
    (elfeed
     :config (progn
               (elfeed-org)
+
+              (defun user/elfeed-search-mark-as-read-and-next ()
+                (interactive)
+                (elfeed-search-untag-all 'unread))
+              (bind-key "N" 'user/elfeed-search-mark-as-read-and-next elfeed-search-mode-map)
+
               (setq-default elfeed-search-filter "@2-weeks-ago +daily +unread ")))
    (elfeed-org
     :defer t
