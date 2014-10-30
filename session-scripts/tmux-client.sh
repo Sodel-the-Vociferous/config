af94975576a6df308438cfc2d214491f2c73de10
Daniel Ralston <daniel@dralston.net>
2014-10-29 20:02:19 -0700

    ALWAYS open a new window in tmux client script
    Basically, so "open Terminal here" works as expected.

--- session-scripts/tmux-client.sh
+++ session-scripts/tmux-client.sh
@@ -8,34 +8,10 @@ not_in_tmux_or_screen () {
     [[ $TERM != screen-256color ]]
 }
 
-no_tmux_clients () {
-    MASTER_GROUP=$(tmux list-sessions |
-        grep "TMUX_MASTER:" |
-        egrep -o "group [0-9]+")
-
-    if [ -z "$MASTER_GROUP" ]
-    then
-        return 0
-    fi
-
-    NUM_ATTACHED=$(tmux list-sessions |
-        grep "$MASTER_GROUP" |
-        grep attached |
-        wc -l)
-
-    [ $NUM_ATTACHED -eq 0 ]
-}
-
 new_tmux_client () {
-    if no_tmux_clients
-    then
-        exec tmux -2 new-session -t TMUX_MASTER \; \
-            set -q destroy-unattached on
-    else
-        exec tmux -2 new-session -t TMUX_MASTER \; \
-            new-window -c $(pwd) \; \
-            set -q destroy-unattached on
-    fi
+    exec tmux -2 new-session -t TMUX_MASTER \; \
+        new-window -c $(pwd) \; \
+        set -q destroy-unattached on
 }
 
 ( [ "$1" = "force" ] || not_in_tmux_or_screen ) &&
