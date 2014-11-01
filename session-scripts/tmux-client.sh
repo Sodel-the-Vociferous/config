5f7dc2b6bf89314c8c994a81515d278c6a244a5b
Daniel Ralston <daniel@dralston.net>
2014-10-31 22:45:07 -0700

    FIX: tmux-client script shouldn't open 2 windows @ first invocation

--- session-scripts/tmux-client.sh
+++ session-scripts/tmux-client.sh
@@ -8,14 +8,31 @@ not_in_tmux_or_screen () {
     [[ $TERM != screen-256color ]]
 }
 
-new_tmux_client () {
-    exec tmux -2 new-session -t TMUX_MASTER \; \
-        new-window -c $(pwd) \; \
-        set -q destroy-unattached on
+tmux_session_running () {
+    tmux list-sessions 2> /dev/null |
+    grep TMUX_MASTER &> /dev/null
 }
 
-( [ "$1" = "force" ] || not_in_tmux_or_screen ) &&
-unset TMUX &&
-export TMUX &&
-tmux -2 new-session -A -s TMUX_MASTER \; detach &&
-new_tmux_client
+if ( [ "$1" = "--force" ] || not_in_tmux_or_screen )
+then
+    unset TMUX
+    export TMUX
+
+    # Creating a new session creates a window
+    # automatically. Connecting to an existing session doesn't. But,
+    # we don't want to create a second window if we're connecting to
+    # the new session that we JUST created -- the session has a fresh
+    # window which we can use!
+    tmux_session_running &&
+    NEW_WINDOW=true ||
+    unset NEW_WINDOW
+
+    tmux -2 new-session -A -s TMUX_MASTER -c "$(pwd)" \; detach
+
+    exec tmux -2 \
+        new-session -t TMUX_MASTER \; \
+        $([[ $NEW_WINDOW ]] && echo new-window -c $(pwd) \;) \
+        set -q destroy-unattached on
+else
+    exit 1
+fi
