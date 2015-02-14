b61069a2f2871112865276fabee1b45c628284be
Daniel Ralston <daniel@dralston.net>
2015-02-13 20:53:14 -0800

    FIX script syntax

--- setup-scripts/big-ext-setup.sh
+++ setup-scripts/big-ext-setup.sh
@@ -6,7 +6,7 @@ then
     exit 1
 fi
 
-FSTAB_LINE = "/dev/disk/by-label/BigExt /media/big-ext              ext4    defaults,auto   0 0"
+FSTAB_LINE="/dev/disk/by-label/BigExt /media/big-ext              ext4    defaults,auto   0 0"
 
 mkdir -p /media/big-ext
 
