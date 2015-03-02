e2be2d6552917736a986b87c6278377f9fe5cfdc
Daniel Ralston <daniel@dralston.net>
2015-03-01 21:22:50 -0800

    Enable services by absolute file path via script
    Systemd doesn't like symlinks. Also, move service files; the path
    doesn't need to be so long now that the directory containing the service
    files doesn't need to be symlinked

--- setup-scripts/services-setup.sh
+++ setup-scripts/services-setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-sudo systemctl disable lightdm
-sudo systemctl enable atd
-sudo systemctl enable avahi-daemon
-sudo systemctl enable dovecot
-sudo systemctl enable gdm
-sudo systemctl enable ntpd
-sudo systemctl enable remote-fs.target
-sudo systemctl enable rpcbind
+systemctl --user enable $HOME/config/systemd-services/emacsd.service
+systemctl --user enable $HOME/config/systemd-services/keychain.service
