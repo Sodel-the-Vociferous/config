cdd3ed4c810b35f790bfa8f38ec067a1f0bb6500
Daniel Ralston <daniel@dralston.net>
2015-06-14 03:28:23 -0700

    Don't allow challenge response ssh auth (same as passwords!)

--- setup-scripts/ssh-setup.sh
+++ setup-scripts/ssh-setup.sh
@@ -3,9 +3,12 @@
 echo "/etc/ssh/sshd_config" |
 xargs -d '\n' readlink -f |
 xargs -d '\n' sudo perl -p -i \
-    -e 's/^(\s*PasswordAuthentication\s+yes)$/#\1/gi' \
+    -e 's/^(\s*PasswordAuthentication\s+)yes$/\1 no/gi' \
     "/etc/ssh/sshd_config"
 
-sudo ack "^\s*PasswordAuthentication\s+no" /etc/ssh/sshd_config >/dev/null ||
-( echo -e "\nPasswordAuthentication no" |
-    sudo tee -a "/etc/ssh/sshd_config" )
+echo "/etc/ssh/sshd_config" |
+xargs -d '\n' readlink -f |
+xargs -d '\n' sudo perl -p -i \
+    -e 's/^(\s*ChallengeResponseAuthentication\s+)yes$/\1 no/gi' \
+    "/etc/ssh/sshd_config"
+
