9b16f77ca517de7ed5754148e26dd461ac4c5378
Daniel Ralston <daniel@dralston.net>
2015-07-12 09:09:32 -0700

    update usershares

--- session-scripts/usershares.sh
+++ session-scripts/usershares.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-net usershare add $USER-share $HOME/share/ "" Everyone:f
 net usershare add $USER-prj $HOME/prj/ "" Everyone:f
+net usershare add $USER-obj $HOME/obj/ "" Everyone:f
 net usershare add $USER-fea /media/big-ext/$USER/fea/ "" Everyone:f
