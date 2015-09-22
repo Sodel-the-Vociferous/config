a305637dead2346af676536b1919409cccab3b24
Daniel Ralston <daniel@dralston.net>
2015-09-22 15:04:30 -0700

    add dralston-shaer usershare

--- session-scripts/usershares.sh
+++ session-scripts/usershares.sh
@@ -2,4 +2,5 @@
 
 net usershare add $USER-prj $HOME/prj/ "" Everyone:f
 net usershare add $USER-obj $HOME/obj/ "" Everyone:f
+net usershare add $USER-share $HOME/share/ "" Everyone:f
 net usershare add $USER-fea /media/big-ext/$USER/fea/ "" Everyone:f
