2840b88d4ed449772ec5e97bc8cf498deb26fc9f
Daniel Ralston <daniel@dralston.net>
2017-06-05 14:52:19 -0700

    use default gnu global backend, not ctags

--- session-scripts/profile.sh
+++ session-scripts/profile.sh
@@ -64,7 +64,7 @@ export PYTHONDONTWRITEBYTECODE
 # Setup gtags w/ exuberant ctags
 GTAGSCONF=$HOME/.globalrc
 export GTAGSCONF
-GTAGSLABEL=ctags
+GTAGSLABEL=default #ctags
 export GTAGSLABEL
 
 if [[ $OS = Windows_NT ]]; then
