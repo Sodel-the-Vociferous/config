6d4de91b8d07141e0168b8b824e909aa1038163d
Daniel Ralston <daniel@dralston.net>
2017-06-21 23:39:33 -0700

    Set DISPLAY env var on WSL

--- session-scripts/bash_profile.sh
+++ session-scripts/bash_profile.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if (uname -r | egrep -e "Microsoft$" &> /dev/null) && [[ -z "$DISPLAY" ]]; then
+   export DISPLAY=":0.0"
+   xset -q &> /dev/null || unset DISPLAY
+fi
+
 . $HOME/.profile
 . $HOME/.bashrc
 if [ -e /home/dralston/.nix-profile/etc/profile.d/nix.sh ]; then . /home/dralston/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
