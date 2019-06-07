f6890f0f0c26ccc9e7ade8d0f65316d7e33b90e8
Daniel Ralston <daniel@dralston.net>
2019-06-07 02:23:34 -0700

    upd symlinks setup script

--- setup-scripts/symlinks-setup.sh
+++ setup-scripts/symlinks-setup.sh
@@ -13,7 +13,8 @@ fi
 source $LIB_ES_PATH
 
 # Stow
-stow --adopt -R -d ~/config/ -t $HOME stow_home
+stow -R -d ~/config/ -t $HOME stow_home
+stow --adopt -d ~/config/ -t $HOME stow_home
 # Copying redshift.conf is a workaround. Redshift can't seem to follow symlinks
 # to its config file.
 cp --remove-destination -ad ~/config/redshift.conf ~/.config/
